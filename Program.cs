e5c61ff489478b3702d81becc1f780e93ae72edf
Kyle Grabski <76755031+kylegrabski@users.noreply.github.com>
2024-06-09 16:29:09 -0700

    init

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using Sharprompt;
+
+var options = new[] { "Option 1", "Option 2", "Option 3" };
+var selectedOption = Prompt.Select("Please select an option:", options);
+
+Console.WriteLine($"You selected: {selectedOption}");
