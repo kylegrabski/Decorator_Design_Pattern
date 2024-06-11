513d946317e2a7eb6e6a2961fed7413cf94c7c34
Kyle Grabski <76755031+kylegrabski@users.noreply.github.com>
2024-06-10 17:02:31 -0700

    invert if statements

--- Program.cs
+++ Program.cs
@@ -46,7 +46,7 @@ while (true)
     Thread.Sleep(1000);
 }
 
-
+return;
 
 
 IUserTask ApplyPriorityTask(IUserTask task)
@@ -58,35 +58,27 @@ IUserTask ApplyPriorityTask(IUserTask task)
         .ToArray();
     var priorityIndex = Prompt.Select("Select Priority: ", priorityOptions);
     var isPriority = Enum.Parse<PriorityEnum>(priorityIndex);
-    if ((isPriority & PriorityEnum.PriorityAssigned) != 0)
-    {
-        return new PriorityTask(task, isPriority);
-    }
-    return task;
+    return (isPriority & PriorityEnum.PriorityAssigned) != 0 ? new PriorityTask(task, isPriority) : task;
 }
 
 IUserTask ApplyRecurringTask(IUserTask task)
 {
     var isRecurringKey = Prompt.Select("Should This Task Recur? ", YES_OR_NO.Keys.ToArray());
     var isRecurring = YES_OR_NO[isRecurringKey];
-    if (isRecurring)
-    {
-        var recurrenceInterval = Prompt.Input<int>("Enter the recurrence interval in seconds");
-        var recurrenceCount = Prompt.Input<int>("Enter the total number of recurrences");
-        return new RecurringTask(task, recurrenceInterval, recurrenceCount);
-    }
-    return task;
+    if (!isRecurring) return task;
+
+    var recurrenceInterval = Prompt.Input<int>("Enter the recurrence interval in seconds");
+    var recurrenceCount = Prompt.Input<int>("Enter the total number of recurrences");
+    return new RecurringTask(task, recurrenceInterval, recurrenceCount);
 }
 
 IUserTask ApplyDeadlineTask(IUserTask task)
 {
     var isDeadlineKey = Prompt.Select("Is There A Deadline? ", YES_OR_NO.Keys.ToArray());
     var isDeadline = YES_OR_NO[isDeadlineKey];
-    if (isDeadline)
-    {
-        Console.WriteLine("YUP, THERE'S A DEADLINE!");
-        var deadline = Prompt.Input<int>("Enter the deadline in seconds");
-        return new DeadlineTask(task, deadline);
-    }
-    return task;
+    if (!isDeadline) return task;
+
+    Console.WriteLine("YUP, THERE'S A DEADLINE!");
+    var deadline = Prompt.Input<int>("Enter the deadline in seconds");
+    return new DeadlineTask(task, deadline);
 }
